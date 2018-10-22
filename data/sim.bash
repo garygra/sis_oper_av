a9e86660a5b0c2caf53161526e5d62f38e4703fa
Edgar Granados <gary.granados@gmail.com>
2018-10-22 11:00:24 -0500

    bash script for simulating multiple cases

--- data/sim.bash
+++ data/sim.bash
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 
-for i in `seq 1 256`;
-do
-
-	../build/livelock_sim
+# for i in `seq 1 256`;
+# do
+i=200
+# while [ $i -gt 0 ]; do
+for i in $(seq 0.1 0.1 5.0); do
+	PACKET_MEANS="$i $i $i $i $i" 
+	# echo $PACKET_MEANS
+	# echo $i
+	../build/livelock_sim --packet_arrival_mean="$PACKET_MEANS" --debug_level=0
 	
-
+	# i=$((i-1))
 done
